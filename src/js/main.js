b7b665e866d69fa02ef308ed07bcbd89ac76d1da
Sheikh Rashed <mdrashed1310@gmail.com>
2021-06-26 00:15:37 +0600

    added layout out of products & categories & fixed swiper js

--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,97 @@
 // Table Contents
 'use strict';
+
+// Swipper Slier
+//  ** Homepage
+/*\ 
+
+==========  Table Contents ============
+
+** Swipper Slider
+  => Homepage 01 - Shipping Cards
+
+
+\*/
+
+// Shipping Slider one
+var shiping = new Swiper('.shipping-container--slider', {
+  spaceBetween: 0,
+  loop: true,
+  loopFillGroupWithBlank: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1200: {
+      slidesPerView: 4,
+    },
+  },
+});
+
+// Categories
+var categories = new Swiper('.popular-categories--slider', {
+  slidesPerView: 1,
+  centeredSlides: true,
+  spaceBetween: 0,
+  loop: true,
+  loopFillGroupWithBlank: true,
+
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+
+  pagination: {
+    el: '.swiper-pagination',
+    dynamicBullets: true,
+  },
+  // breakpoints: {
+  //   0: {
+  //     slidesPerView: 1,
+  //   },
+  //   768: {
+  //     slidesPerView: 2,
+  //   },
+  //   1200: {
+  //     slidesPerView: 4,
+  //   },
+  // },
+});
+
+// Shipping Slider one
+var products = new Swiper('.popular-products--slider', {
+  slidesPerView: 'auto',
+  // centeredSlides: true,
+  spaceBetween: 15,
+  loop: true,
+  loopFillGroupWithBlank: true,
+
+  // autoplay: {
+  //   delay: 2500,
+  //   disableOnInteraction: false,
+  // },
+
+  pagination: {
+    el: '.swiper-pagination',
+    dynamicBullets: true,
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    575: {
+      slidesPerView: 2,
+    },
+  },
+});
