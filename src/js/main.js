cb7c376af653156010697193c4ee06cb04b72c36
Sheikh Rashed <mdrashed1310@gmail.com>
2021-07-01 16:40:26 +0600

    Homepage 01 brand icons added border and removed extra space

--- src/js/main.js
+++ src/js/main.js
@@ -353,10 +353,10 @@ var brandsName = new Swiper('.brand-name-slide--one', {
   loop: true,
   loopFillGroupWithBlank: true,
   autoHeight: true,
-  autoplay: {
-    delay: 2500,
-    disableOnInteraction: false,
-  },
+  // autoplay: {
+  //   delay: 2500,
+  //   disableOnInteraction: false,
+  // },
   pagination: {
     el: '.swiper-pagination',
     clickable: true,
