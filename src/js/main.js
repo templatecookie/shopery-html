94b9a86129cad7f25eb93f4af326619446a63000
Sheikh Rashed <mdrashed1310@gmail.com>
2021-07-16 04:21:59 +0600

    mobile menu updated

--- src/js/main.js
+++ src/js/main.js
@@ -27,27 +27,67 @@
  13.Blogs-list
  14.Shops
  
-\/\/\/\/\/\/\/\*/
+
+
+ \/\/\/\/\/\/\/\*/
+
+//  Close Sidebar
+let closeBar = document.querySelector('.header__cross');
+let mobileSidebar = document.querySelector('.header__sidebar');
+let menuBtn = document.querySelector('.header__sidebar-btn');
+const body = document.querySelector('body');
+
+// Open
+menuBtn.addEventListener('click', function () {
+  mobileSidebar.classList.add('active');
+  body.classList.add('overlay');
+});
+
+// close
+closeBar.addEventListener('click', function () {
+  mobileSidebar.classList.remove('active');
+  body.classList.remove('overlay');
+});
+
+var navMenu = [].slice.call(
+  document.querySelectorAll('.header__mobile-menu-item')
+);
+
+for (var y = 0; y < navMenu.length; y++) {
+  navMenu[y].addEventListener('click', function () {
+    menuClick(this);
+  });
+}
+
+function menuClick(current) {
+  const active = current.classList.contains('active');
+  navMenu.forEach((el) => el.classList.remove('active'));
+  if (active) {
+    current.classList.remove('active');
+  } else {
+    current.classList.add('active');
+  }
+}
 
 // Modal
 $(document).ready(function () {
   $('#newsletter').modal('show');
 
-  $('#countdown').syotimer({
-    year: 2021,
-    month: 7,
-    day: 25,
-    hour: 20,
-    minute: 30,
-  });
+  // $('#countdown').syotimer({
+  //   year: 2021,
+  //   month: 7,
+  //   day: 25,
+  //   hour: 20,
+  //   minute: 30,
+  // });
 
-  $('#countdownTwo').syotimer({
-    year: 2021,
-    month: 7,
-    day: 25,
-    hour: 20,
-    minute: 30,
-  });
+  // $('#countdownTwo').syotimer({
+  //   year: 2021,
+  //   month: 7,
+  //   day: 25,
+  //   hour: 20,
+  //   minute: 30,
+  // });
 });
 
 //====== Banners Slider ====== \\
